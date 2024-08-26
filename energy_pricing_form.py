8ee774dc5edf484656b57f70e04e39741c58b78c
jcamou-su <jeronimocamou34@gmail.com>
2024-08-26 18:42:28 -0400

    Fixed session state initialization issue

--- energy_pricing_form.py
+++ energy_pricing_form.py
@@ -39,4 +39,3 @@ if submit_button:
     new_data = [supplier_name, price1, price2, price3, price4, price5, price6, price7, price8, notes]
     sheet.append_row(new_data)
     st.success("Thank you! Your submission has been received.")
-
