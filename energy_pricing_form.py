ca855883105e87a747fe14d991dcd6490fce5500
jcamou-su <jeronimocamou34@gmail.com>
2024-09-11 17:53:31 -0400

    Fixed session state initialization issue

--- energy_pricing_form.py
+++ energy_pricing_form.py
@@ -23,21 +23,16 @@ st.title("Stanwich Energy")
 
 # Form for adding new supplier data
 with st.form(key="supplier_form"):
-    supplier_name = st.text_input(label="Company Name")
+    supplier_name = st.text_input(label="Bet")
     price1 = st.number_input('Enter Price 1', min_value=0.0, format="%.6f")
     price2 = st.number_input('Enter Price 2', min_value=0.0, format="%.6f")
     price3 = st.number_input('Enter Price 3', min_value=0.0, format="%.6f")
-    price4 = st.number_input('Enter Price 4', min_value=0.0, format="%.6f")
-    price5 = st.number_input('Enter Price 5', min_value=0.0, format="%.6f")
-    price6 = st.number_input('Enter Price 6', min_value=0.0, format="%.6f")
-    price7 = st.number_input('Enter Price 7', min_value=0.0, format="%.6f")
-    price8 = st.number_input('Enter Price 8', min_value=0.0, format="%.6f")
     notes = st.text_input(label="Additional Notes")
     
     submit_button = st.form_submit_button(label="Submit Supplier Details")
 
 if submit_button:
-    new_data = [supplier_name, price1, price2, price3, price4, price5, price6, price7, price8, notes]
+    new_data = [supplier_name, price1, price2, price3,notes]
     sheet.append_row(new_data)
     st.success("Thank you! Your submission has been received.")
 
